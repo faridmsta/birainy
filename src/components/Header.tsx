6cc623275c813ab6c4cf8fc1679a4f17a315635e
faridmsta <faridmustafyev@gmail.com>
2025-05-08 03:00:54 +0400

    Animated Hamburger Added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -76,7 +76,7 @@ const Header = () => {
           <div className="mobile_right flex gap-2 mtb:hidden" >
             <LangBtn />
             <div onClick={() => setIsMobileOpen((prev) => !prev)} className="hamburger py-[2px] px-2 bg-primary rounded-[8px]">
-              <Hamburger />
+              <AnimatedHamburger isOpen={isMobileOpen} />
             </div>
           </div>
           <div className="hidden mtb:flex items-center justify-between bg-gray-light flex-1 py-[0.75em] px-[1.25em] xl:py-[0.875em] xl:px-[1.75em] rounded-[16px]">
@@ -168,15 +168,6 @@ const Header = () => {
 
 export default Header
 
-const Hamburger = () => (
-  <div className="w-[34px]  h-[34px] flex items-center justify-center ">
-    <div className="flex flex-col items-center justify-between h-[16px]">
-      {Array(3).fill("_").map((_, index) => (
-        <span key={"hamburgerSpan" + index} className='h-[1px] bg-dark w-[21px] '></span>
-      ))}
-    </div>
-  </div>
-)
 
 const ContactUs = () => (
   <Link href={""} className='bg-primary rounded-[8px]'>
@@ -185,3 +176,46 @@ const ContactUs = () => (
     </button>
   </Link>
 )
+
+const AnimatedHamburger = ({ isOpen }: { isOpen: boolean }) => {
+  return (
+    <svg
+      width="35"
+      height="35"
+      viewBox="0 0 100 100"
+      className="cursor-pointer"
+    >
+      <path
+        className={`transition-[stroke-dashoffset] duration-500 ease-in-out origin-center  ${
+          isOpen ? "" : ""
+        }`}
+        d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
+        stroke="black"
+        strokeWidth="6"
+        fill="none"
+        strokeDasharray="60 140"
+        strokeDashoffset={isOpen ? "-140" : "0"}
+      />
+      <path
+        className={`transition-all duration-500 ease-in-out ${
+          isOpen ? "translate-x-full " : "translate-x-0"
+        }`}
+        d="M 20,50 H 80"
+        stroke="black"
+        strokeWidth="6"
+        fill="none"
+      />
+      <path
+        className={`transition-[stroke-dashoffset] duration-500 ease-in-out origin-center ${
+          isOpen ? "" : ""
+        }`}
+        d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
+        stroke="black"
+        strokeWidth="6"
+        fill="none"
+        strokeDasharray="60 140"
+        strokeDashoffset={isOpen ? "-140" : "0"}
+      />
+    </svg>
+  );
+};
